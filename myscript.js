e67474b6fff6d6f5b6c00403617b1d0c17a6be26
enrico76 <enricodip@gmail.com>
2019-07-29 18:59:47 +0200

    added ajax callback and function to compare number on click recall css style

--- myscript.js
+++ myscript.js
@@ -2,3 +2,34 @@
 //  che prende un numero random da 1 a 9.
 // Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
 // Il numero ottenuto appare al centro del quadrato
+
+$(document).ready(function(){
+  // richiamo api dentro una variabile
+  var apiNumrdm = "https://flynn.boolean.careers/exercises/api/random/int";
+
+  $(".quadrato").click(function(){
+    var quadsel = $(this);
+    console.log(quadsel);
+
+    $.ajax ({
+
+      url : apiNumrdm,
+      method : "GET",
+      success : function (data){
+        var apiNum = data.response;
+
+        if (apiNum > 5) {
+          quadsel.addClass("green");
+          quadsel.text(apiNum);
+        } else {
+          quadsel.addClass("yellow");
+          quadsel.text(apiNum);
+        };
+      },
+      error: function(richiesta,stato,errore){
+               console.log("c'è un problema con il server",richiesta,stato,errore);
+             }
+    });
+  });
+
+});
